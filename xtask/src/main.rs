d32bf14ba70ce5e07c52c2251141c390d2e9d89c
FlintWithBlackCrown <kbolokhov@gmail.com>
2026-08-21 15:47:16 +0300

    add new fitness function

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,4 @@
+use cargo_metadata::MetadataCommand;
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -68,5 +69,37 @@ fn run_fitness_checks() -> Result<(), Box<dyn std::error::Error>> {
         return Err("Dependencies topoly is invalid".into());
     }
 
+    check_app_core_isolation()?;
+
+    Ok(())
+}
+
+fn check_app_core_isolation() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Check isolation for app_core...");
+
+    let metadata = MetadataCommand::new().exec()?;
+
+    let app_core_pkg = metadata
+        .workspace_packages()
+        .into_iter()
+        .find(|pkg| pkg.name == "app_core")
+        .ok_or("Crate `app_core` not found!")?;
+
+    let other_workspace_crates: Vec<String> = metadata
+        .workspace_packages()
+        .into_iter()
+        .map(|pkg| pkg.name.clone())
+        .filter(|name| name != "app_core")
+        .collect();
+
+    for dep in &app_core_pkg.dependencies {
+        if other_workspace_crates.contains(&dep.name) {
+            eprintln!("❌ FITNESS FUNCTION FAILED: `app_core` violates architecture!");
+            eprintln!("   Found banned addiction: `{}`", dep.name);
+            std::process::exit(1);
+        }
+    }
+
+    println!("    `app_core` isolated from other application crates!.\n");
     Ok(())
 }
